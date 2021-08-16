8037bbbbc3a234b910ea84d855bf963764010f40
z0ji <zoji@maelstrom.finance>
2021-08-15 20:49:22 -0700

    decimals: reworked add implementation and implemented sub

--- src/decimal.rs
+++ src/decimal.rs
@@ -1,4 +1,5 @@
 use std::{
+    cmp,
     cmp::{PartialEq, Eq, PartialOrd, Ord, Ordering},
     ops::{Add, AddAssign, Sub, SubAssign, Mul, MulAssign, Div, DivAssign},
     fmt,
@@ -13,9 +14,16 @@ pub enum DecimalError {
     MaxDecimalsExceeded,
 }
 
-//all math in here is implemented in such a way that all operations that *aren't*
-// explicitly checked_* (i.e. all the inline ops like +,-,*,/,%, etc.) should never
-// be able to fail and could hence be replaced by unsafe_* calls to reduce strain on compute budget
+const KEEP_MAX_DECIMALS_DEFAULT: bool = true;
+
+// all math in here is implemented in such a way that all operations that *aren't* ...
+//  ... explicitly checked_* (i.e. all the inline ops like +,-,*,/,%, etc.) should never ...
+//  ... be able to fail and could hence be replaced by unsafe_* calls to reduce strain on compute budget
+//
+// KEEP_MAX_DECIMALS = true ensures that after all operations it holds that ...
+//  ... result.decimals == max(operand1.decimals, operand2.decimals)
+//  KEEP_MAX_DECIMALS = false otoh means result.decimals can be different from either operators
+//  Notice that KEEP_MAX_DECIMALS means potentially lower precision in case of division!
 macro_rules! unsigned_decimal {(
     $name:ident,
     $value_type:ty,
@@ -54,7 +62,7 @@ macro_rules! unsigned_decimal {(
         }
 
         pub fn fract(&self) -> $value_type {
-            self.decimals % Self::ten_to_the(self.decimals)
+            self.value % Self::ten_to_the(self.decimals)
         }
 
         //reduce decimals as to eliminate all trailing zeros
@@ -71,75 +79,86 @@ macro_rules! unsigned_decimal {(
             }
         }
 
-        pub fn checked_add(self, other: Self) -> Option<Self> {
-            if self.decimals == other.decimals {
-                match self.value.checked_add(other.value) {
-                    Some(value) => Some(Self{value: value, decimals: self.decimals}),
-                    None => {
-                        if self.decimals == 0 {
-                            return None;
-                        }
-                        let trunc = (self.trunc() + other.trunc());
-                        let fract = (self.fract() + other.fract());
-                        Some(Self{value: trunc*Self::ten_to_the(self.decimals-1) + fract/10, decimals: self.decimals-1})
-                    }
-                }
+        fn equalize_decimals<const KEEP_MAX_DECIMALS: bool>(v1: Self, v2: Self) -> Option<($value_type, $value_type, u8)> {
+            if v1.decimals == v2.decimals {
+                //special handling to optimize and simplify typical case
+                Some((v1.value, v2.value, v1.decimals))
             }
             else {
-                let (fewer_dec, more_dec) = if self.decimals < other.decimals {(&self, &other)} else {(&other, &self)};
+                let v1_has_fewer_decimals = v1.decimals < v2.decimals;
+                let (fewer_dec, more_dec) = if v1_has_fewer_decimals {(&v1, &v2)} else {(&v2, &v1)};
+                let dec_diff = more_dec.decimals - fewer_dec.decimals;
+                let (_, ubud) = Self::get_bound_unused_decimals(fewer_dec.value);
+                let mut shift = cmp::min(ubud, dec_diff);
+                if KEEP_MAX_DECIMALS && shift != dec_diff {
+                    return None;
+                }
 
-                let (fract, decimals) = {
-                    let dec_diff = more_dec.decimals - fewer_dec.decimals;
-                    let fewer_frac = fewer_dec.fract();
-                    let more_frac = more_dec.fract();
-                    //following checked_add can only possibly fail if decimails == MAX_DECIMALS for self or other
-                    match (fewer_frac*Self::ten_to_the(dec_diff)).checked_add(more_frac) {
-                        Some(value) => (value, more_dec.decimals),
-                        None => (fewer_frac*Self::ten_to_the(dec_diff-1) + more_frac/10, more_dec.decimals-1)
+                let shifted_fewer_value = match fewer_dec.value.checked_mul(Self::ten_to_the(shift)) {
+                    Some(value) => value,
+                    None => {
+                        if KEEP_MAX_DECIMALS {
+                            return None;
+                        }
+                        shift -= 1;
+                        fewer_dec.value * Self::ten_to_the(shift)
                     }
                 };
+                let shifted_more_value = more_dec.value/Self::ten_to_the(dec_diff-shift);
 
-                let fewer_trunc = fewer_dec.trunc();
-                let more_trunc_with_carry = {
-                    let carry_over = fract/Self::ten_to_the(decimals); //either 0 or 1
-                    more_dec.trunc() + carry_over //always safe since more_dec.decimals is >= 1
-                };
-                let fract = fract % Self::ten_to_the(decimals); //eliminate carry from fract if it exists
-
-                //following checked_add can only possibly fail if decimals == 0 for self or other
-                let trunc = match fewer_trunc.checked_add(more_trunc_with_carry) {
-                    Some(value) => value,
-                    None => {return None;}
-                };
-                
-                let lbud = Self::get_lower_bound_unused_decimals(trunc);
-                let trunc_with_all_dec = trunc.checked_mul(Self::ten_to_the(decimals));
-                if lbud >= decimals || (lbud+1 == decimals && trunc_with_all_dec.is_some()) {
-                    //block: we can fit all decimals into the result
-                    Some(Self{value: trunc_with_all_dec.unwrap() + fract, decimals})
-                }
-                else {
-                    //block: we have to truncate some decimals
-                    debug_assert!(trunc != 0);
-                    debug_assert!((lbud as u32) < Self::BITS); //should be a logical consequence of the previous line
-                    let unused_decimals = lbud + if trunc.checked_mul(Self::ten_to_the(lbud+1)).is_some() {1} else {0};
-                    let value = trunc*Self::ten_to_the(unused_decimals) +
-                                fract/Self::ten_to_the(decimals-unused_decimals);
-                    Some(Self{value, decimals: unused_decimals})
-                }
+                Some(
+                    if v1_has_fewer_decimals {
+                        (shifted_fewer_value, shifted_more_value, fewer_dec.decimals + shift)
+                    }
+                    else {
+                        (shifted_more_value, shifted_fewer_value, fewer_dec.decimals + shift)
+                    }
+                )
             }
         }
 
-        pub fn checked_sub(self, _other: Self) -> Option<Self> {
+        pub fn checked_add<const KEEP_MAX_DECIMALS: bool>(self, other: Self) -> Option<Self> {
+            match Self::equalize_decimals::<KEEP_MAX_DECIMALS>(self, other) {
+                Some((val_1, val_2, decimals)) => {
+                    match val_1.checked_add(val_2) {
+                        Some(value) => Some(Self{value, decimals}),
+                        None => {
+                            if KEEP_MAX_DECIMALS || decimals == 0 {
+                                return None;
+                            }
+                            let value = (val_1/10 + val_2/10) + (val_1%10 + val_2%10)/10;
+                            Some(Self{value, decimals: decimals-1})
+                        }
+                    }
+                },
+                None => None
+            }
+        }
+
+        pub fn checked_sub<const KEEP_MAX_DECIMALS: bool>(self, other: Self) -> Option<Self> {
+            match Self::equalize_decimals::<KEEP_MAX_DECIMALS>(self, other) {
+                Some((val_1, val_2, decimals)) => {
+                    if val_1 < val_2 {
+                        Some(Self{value: val_1 - val_2, decimals})
+                    }
+                    else {
+                        None
+                    }
+                },
+                None => None
+            }
+        }
+
+        pub fn checked_mul<const KEEP_MAX_DECIMALS: bool>(self, _other: Self) -> Option<Self> {
             todo!()
         }
 
-        pub fn checked_mul(self, _other: Self) -> Option<Self> {
+        pub fn checked_div<const KEEP_MAX_DECIMALS: bool>(self, _other: Self) -> Option<Self> {
             todo!()
         }
 
-        pub fn checked_div(self, _other: Self) -> Option<Self> {
-            todo!()
+        pub const fn ten_to_the(exp: u8) -> $value_type {
+            Self::TEN_TO_THE[exp as usize]
         }
 
         const fn create_ten_to_the() -> [$value_type; (Self::MAX_DECIMALS+1) as usize] {
@@ -157,42 +176,49 @@ macro_rules! unsigned_decimal {(
         }
 
         const TEN_TO_THE: [$value_type; (Self::MAX_DECIMALS+1) as usize] = Self::create_ten_to_the();
-
-        fn ten_to_the(exp: u8) -> $value_type {
-            Self::TEN_TO_THE[exp as usize]
-        }
         
         //determines the number of decimal places that are definitely unused given the number of leading
         // zeros in the binary representation
         //
         //e.g.:
+        // multiplying by 10 = 8 + 2 = 0b1010 -> x*10 = x*8 + x*2 = x<<3 + x<<1
         // an unsized integer with at least 4 leading zeros can always be multiplied by 10 without risk of overflow
         // an unsized integer with 2 or fewer leading zeros can never be safely multiplied by 10
         // an unsized integer with 3 leading zeros might or might not be safely multiplied by 10:
-        // b*10 = b*8 + b*2 = b<<3 + b<<1
         // 0b0001_0000 * 10 = 0b1010_0000 -> safe to multiply by 10
         // 0b0001_1100 * 10 = 0b1110_0000 + 0b0011_1000 -> overflow
-        const fn create_loader_bound_unused_decimals() -> [u8; (Self::BITS + 1) as usize] {
-            let mut tzbl = [Self::MAX_DECIMALS; (Self::BITS + 1) as usize];
-            tzbl[0] = 0;
+        // 
+        // multiplying by 10^2 = 64 + 32 + 4 = 0b1100100 -> x*100 = x*64 + x*32 + x*4 = x<<6 + x<<5 + x<<2
+        // so again 6 leading zeros might or might not be enough to multiply by 100 while ...
+        // ... 5 definitely isn't enough and 7 certainly is
+        // 
+        // same for multiplying by 10^3 = 0b1111101000 -> x*1000 = x<<9 + ... so 9 might or might not be enough
+        //
+        // however multiplying by 10^4 = 0b10011100010000 -> x*10000 = x<<13 + ... so 13 not 12!
+        const fn get_bound_unused_decimals(value: $value_type) -> (u8,u8) {
+            let zeros = value.leading_zeros() as usize;
+            (Self::BOUND_UNUSED_DECIMALS[zeros], Self::BOUND_UNUSED_DECIMALS[zeros+1])
+        }
+        const BOUND_UNUSED_DECIMALS: [u8; (Self::BITS + 2) as usize] = Self::create_bound_unused_decimals();
+        const fn create_bound_unused_decimals() -> [u8; (Self::BITS + 2) as usize] {
+            let mut bud = [Self::MAX_DECIMALS; (Self::BITS + 2) as usize];
+            bud[0] = 0;
             let mut pot: $value_type = 10;
-            let mut val: $value_type = 1;
+            let mut val: $value_type = 1 << 1; //essentially we start with the second iteration
             let mut i = 1;
             loop { //const functions can't use for loops
-                if i == Self::BITS as usize {
-                    break;
+                let jump = (val / pot) as u8;
+                if jump == 1 {
+                    if pot == Self::ten_to_the(Self::MAX_DECIMALS) {
+                        break;
+                    }
+                    pot *= 10;
                 }
-                tzbl[i] = tzbl[i-1] + if val / pot != 0 {pot *= 10; 1} else {0};
+                bud[i] = bud[i-1] + jump;
                 val <<= 1;
                 i += 1;
             }
-            tzbl
-        }
-
-        const LOWER_BOUND_UNUSED_DECIMALS: [u8; (Self::BITS + 1) as usize] = Self::create_loader_bound_unused_decimals();
-
-        fn get_lower_bound_unused_decimals(value: $value_type) -> u8 {
-            Self::LOWER_BOUND_UNUSED_DECIMALS[value.leading_zeros() as usize]
+            bud
         }
     }
 
@@ -238,7 +264,8 @@ macro_rules! unsigned_decimal {(
         type Output = Self;
 
         fn add(self, other: Self) -> Self::Output {
-            self.checked_add(other).unwrap_or_else(|| panic!("Overflow while adding decimals {:?} {:?}", self, other))
+            self.checked_add::<KEEP_MAX_DECIMALS_DEFAULT>(other)
+                .unwrap_or_else(|| panic!("Overflow while adding {:?} {:?}", self, other))
         }
     }
 
@@ -252,7 +279,8 @@ macro_rules! unsigned_decimal {(
         type Output = Self;
 
         fn sub(self, other: Self) -> Self::Output {
-            self.checked_sub(other).unwrap_or_else(|| panic!("Underflow while subtracting decimals {:?} {:?}", self, other))
+            self.checked_sub::<KEEP_MAX_DECIMALS_DEFAULT>(other)
+                .unwrap_or_else(|| panic!("Underflow while subtracting {:?} {:?}", self, other))
         }
     }
 
@@ -266,7 +294,8 @@ macro_rules! unsigned_decimal {(
         type Output = Self;
 
         fn mul(self, other: Self) -> Self::Output {
-            self.checked_mul(other).unwrap_or_else(|| panic!("Overflow while multiplying decimals {:?} {:?}", self, other))
+            self.checked_mul::<KEEP_MAX_DECIMALS_DEFAULT>(other)
+                .unwrap_or_else(|| panic!("Overflow while multiplying {:?} {:?}", self, other))
         }
     }
 
@@ -280,7 +309,8 @@ macro_rules! unsigned_decimal {(
         type Output = Self;
 
         fn div(self, other: Self) -> Self::Output {
-            self.checked_div(other).unwrap_or_else(|| panic!("Division by zero while dividing decimals {:?} {:?}", self, other))
+            self.checked_div::<KEEP_MAX_DECIMALS_DEFAULT>(other)
+                .unwrap_or_else(|| panic!("Division by zero while dividing {:?} {:?}", self, other))
         }
     }
 
@@ -317,12 +347,19 @@ macro_rules! unsigned_decimal {(
 //     19,
 // }
 
-// unsigned_decimal! {
-//     DecimalU32,
-//     u32,
-//     32,
-//     9,
-// }
+unsigned_decimal! {
+    DecimalU32,
+    u32,
+    32,
+    9,
+}
+
+unsigned_decimal! {
+    DecimalU16,
+    u16,
+    16,
+    4,
+}
 
 unsigned_decimal! {
     DecimalU8,
@@ -341,7 +378,16 @@ mod tests {
         assert_eq!(new(111,2)+new(11,1), new(221,2));
         assert_eq!(new(127,0)+new(128,0), new(255,0));
         assert_eq!(new(127,2)+new(128,2), new(255,2));
-        assert_eq!(new(128,2)+new(128,2), new(25,1));
-        assert!(new(128,0).checked_add(new(128,0)).is_none());
+        assert_eq!(new(128,2).checked_add::<false>(new(128,2)), Some(new(25,1)));
+        assert!(new(128,2).checked_add::<true>(new(128,2)).is_none());
+        assert!(new(128,0).checked_add::<false>(new(128,0)).is_none());
     }
+
+    // #[test]
+    // fn print_bounds() {
+    //     print!("Bound:");
+    //     for i in 0..=33 {
+    //         print!("{}", DecimalU32::BOUND_UNUSED_DECIMALS[i]);
+    //     }
+    // }
 }
