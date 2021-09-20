bd18845d10e41be2f02d7949c0f48bf0753004a2
rowdyear <rowdyear@maelstrom.finance>
2021-09-20 13:23:32 -0700

    more test program_ids for testing deployments

--- src/lib.rs
+++ src/lib.rs
@@ -8,5 +8,6 @@ mod invariant;
 pub mod pool_fee;
 pub mod processor;
 pub mod state;
-//solana_program::declare_id!("4uQeVj5tqViQh7yWWGStvkEG1Zmhx6uasJtWCJziofM");
-solana_program::declare_id!("DF7HRRECYMAkkwanarcantNwnC7ehiw8sD53u5ZBMUyM");
+solana_program::declare_id!("4uQeVj5tqViQh7yWWGStvkEG1Zmhx6uasJtWCJziofM");
+//solana_program::declare_id!("DF7HRRECYMAkkwanarcantNwnC7ehiw8sD53u5ZBMUyM");
+//solana_program::declare_id!("6tWRQFov1NU1NmgQpd2WzN4RoPcR9MMTMt1pWAJtoCTF");
