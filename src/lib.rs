6d93f596f882882a6544ab4696dc68bc9a31942b
Alick <alick@exsphere.com>
2022-05-05 15:23:01 -0400

    Add security_txt!

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,22 @@ pub mod state;
 
 pub const TOKEN_COUNT: usize = 6;
 
+use solana_security_txt::security_txt;
+
+security_txt! {
+    // Required fields
+    name: "Swim.io",
+    project_url: "https://swim.io/",
+    contacts: "email:admin@swim.io",
+    policy: "https://swim.io/security",
+
+    // Optional fields
+    preferred_languages: "en",
+    encryption: "https://swim.io/pgp-key.txt",
+    expiry: "2026-04-28T05:00:00.000Z",
+    auditors: "Kudelski"
+}
+
 // 4 Pool
 //solana_program::declare_id!("SWiMBJS9iBU1rMLAKBVfp73ThW1xPPwKdBHEU2JFpuo");
 
